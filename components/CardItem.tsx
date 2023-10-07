b81878c245b860665e7c1197799948619d996734
MrLatif <lertekin21@epoka.edu.al>
2023-10-07 20:35:34 +0200

    lastest changes

--- components/CardItem.tsx
+++ components/CardItem.tsx
@@ -5,41 +5,51 @@ import {
 import React from "react";
 import Image from "next/image";
 import { DataProps } from "../types";
+import { Draggable } from "react-beautiful-dnd";
 
 const CardItem = ({ data }: any) => {
   return (
-    <div className="bg-white rounded-md p-3 mt-3 ">
-      <label
-        className={`bg-gradient-to-r from-blue-500 to-blue-200 px-2 py-1 rounded text-white text-sm
-      ${
-        data.priority === 0
-          ? "from-blue-500 to-blue-200"
-          : data.priority === 1
-          ? "from-green-500 to-green-200"
-          : "from-red-500 to-red-200"
-      }
-      `}
-      >
-        {data.priority === 0
-          ? "Low Priortity"
-          : data.priority === 1
-          ? "Medium Priortity"
-          : "High Priortity"}
-      </label>
-      <h5 className="text-md my-3 text-lg leading-6">{data.title}</h5>
-      <div className="flex justify-between">
-        <div className="flex space-x-4 items-center">
-          <span className="flex space-x-2 items-center">
-            <ChatBubbleLeftRightIcon className="w-4 h-4 text-gray-500" />
-            <span>{data.chat}</span>
-          </span>
-          <span className="flex space-x-2 items-center">
-            <PaperClipIcon className="w-4 h-4 text-gray-500" />
-            <span>{data.attachment}</span>
-          </span>
+    <Draggable index={data.id} draggableId={data.id.toString()}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="bg-white rounded-md p-3 mt-3 "
+        >
+          <label
+            className={`bg-gradient-to-r from-blue-500 to-blue-200 px-2 py-1 rounded text-white text-sm
+        ${
+          data.priority === 0
+            ? "from-blue-500 to-blue-200"
+            : data.priority === 1
+            ? "from-green-500 to-green-200"
+            : "from-red-500 to-red-200"
+        }
+        `}
+          >
+            {data.priority === 0
+              ? "Low Priortity"
+              : data.priority === 1
+              ? "Medium Priortity"
+              : "High Priortity"}
+          </label>
+          <h5 className="text-md my-3 text-lg leading-6">{data.title}</h5>
+          <div className="flex justify-between">
+            <div className="flex space-x-4 items-center">
+              <span className="flex space-x-2 items-center">
+                <ChatBubbleLeftRightIcon className="w-4 h-4 text-gray-500" />
+                <span>{data.chat}</span>
+              </span>
+              <span className="flex space-x-2 items-center">
+                <PaperClipIcon className="w-4 h-4 text-gray-500" />
+                <span>{data.attachment}</span>
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
--- components/Kanban.tsx
+++ components/Kanban.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import {
   ChevronDownIcon,
   EllipsisVerticalIcon,
 } from "@heroicons/react/24/solid";
-import {
-  ChatBubbleLeftRightIcon,
-  PaperClipIcon,
-} from "@heroicons/react/24/outline";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import { Paper } from "@mui/material";
 import CardItem from "./CardItem";
 import BoardData from "../data/board-data.json";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const Kanban = () => {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    if (process.browser) {
+      setReady(true);
+    }
+  }, []);
   return (
     <>
       <div className="p-10">
@@ -20,28 +26,47 @@ const Kanban = () => {
           <h4 className="text-4xl font-bold text-gray-600">Todo List</h4>
           <ChevronDownIcon className="w-9 h-9 text-gray-500 rounded-full p-1 bg-white ml-5 shadow-xl" />
         </div>
-        {/* Board columns */}
-        <div className="grid grid-cols-4 gap-5 my-5">
-          {BoardData.map((board, index) => {
-            return (
-              <div
-                key={index}
-                className="bg-gray-100 p-3 rounded-md shadow-md flex flex-col relative overflow-hidden"
-              >
-                <span className="w-full h-1 bg-gradient-to-r from-pink-700 to-red-200 absolute inset-x-0 top-0"></span>
-                <h4 className="flex justify-between items-center mb-2">
-                  <span className="text-2xl text-gray-600">{board.name}</span>
-                  <EllipsisVerticalIcon className="w-5 h-5 text-gray-500" />
-                </h4>
 
-                {board.items.length > 0 &&
-                  board.items.map((item, iIndex) => {
-                    return <CardItem key={iIndex} data={item} />;
-                  })}
-              </div>
-            );
-          })}
-        </div>
+        {/* Board columns */}
+        {ready && (
+          <DragDropContext>
+            <div className="grid grid-cols-4 gap-5 my-5">
+              {BoardData.map((board, index) => {
+                return (
+                  <div key={index}>
+                    <Droppable droppableId={board.name}>
+                      {(provider) => (
+                        <div
+                          {...provider.droppableProps}
+                          ref={provider.innerRef}
+                          className="bg-gray-100 p-3 rounded-md shadow-md flex flex-col relative overflow-hidden"
+                        >
+                          <span className="w-full h-1 bg-gradient-to-r from-pink-700 to-red-200 absolute inset-x-0 top-0"></span>
+                          <h4 className="flex justify-between items-center mb-2">
+                            <span className="text-2xl text-gray-600">
+                              {board.name}
+                            </span>
+                            <EllipsisVerticalIcon className="w-5 h-5 text-gray-500" />
+                          </h4>
+
+                          {board.items.length > 0 &&
+                            board.items.map((item, iIndex) => {
+                              return (<CardItem key={iIndex} data={item} />);
+                            })}
+
+                          <button className="flex justify-center items-center mt-6 space-x-2 text-lg">
+                            <span>Add task</span>
+                            <PlusCircleIcon className="w-6 h-6 text-gray-500" />
+                          </button>
+                        </div>
+                      )}
+                    </Droppable>
+                  </div>
+                );
+              })}
+            </div>
+          </DragDropContext>
+        )}
       </div>
     </>
   );
