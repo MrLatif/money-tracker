4274f8284007cf9851b964b86a44fdbb0ba39b4d
Brlr2003 <omaralsafarti03@gmail.com>
2023-10-13 17:51:40 +0200

    connect to mebzagency db

--- lib/firebase/index.tsx
+++ lib/firebase/index.tsx
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDY66WCkQ366_XEwF_fZ0Ca8xPsjxUvtT8",
-  authDomain: "money-tracker-37bff.firebaseapp.com",
-  projectId: "money-tracker-37bff",
-  storageBucket: "money-tracker-37bff.appspot.com",
-  messagingSenderId: "250886990885",
-  appId: "1:250886990885:web:46f61baf8ac978490aa3cc",
+  apiKey: "AIzaSyCBNjUOti848Ax557rWXPiy917frI26Quc",
+  authDomain: "money-tracker-14be5.firebaseapp.com",
+  projectId: "money-tracker-14be5",
+  storageBucket: "money-tracker-14be5.appspot.com",
+  messagingSenderId: "156411479424",
+  appId: "1:156411479424:web:888e6c355b9de36af1ad65",
 };
 
 // Initialize Firebase
