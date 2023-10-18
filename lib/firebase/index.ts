09d3bc54ec1b1ed9f68b81405cab811f21c2be8a
MrLatif <lertekin21@epoka.edu.al>
2023-10-18 22:50:51 +0200

    fixed the error: element  type is invalid

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,12 +12,11 @@ const firebaseConfig = {
   projectId: "money-tracker-14be5",
   storageBucket: "money-tracker-14be5.appspot.com",
   messagingSenderId: "156411479424",
-  appId: "1:156411479424:web:888e6c355b9de36af1ad65"
+  appId: "1:156411479424:web:888e6c355b9de36af1ad65",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { app, db, auth };
+export { app, db };
